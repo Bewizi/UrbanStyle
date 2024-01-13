5074403acac18018aa174f52a32300a3d736608d
Bewizi <toluwalase.bewaji@yahoo.com>
2024-01-13 17:19:33 +0100

    bewizi

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 import { Flex, Side } from "../components/basic/flex";
 import { Container } from "../components/container";
 import { Navigation } from "../components/navigation";
 import { Footer } from "./home/footer";
+import { Button } from "../components/button";
 
 export const AboutUS = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
   return (
     <>
       <Container className="com:container com:mx-auto mb-20 mt-5 sm:px-5">
@@ -18,11 +26,12 @@ export const AboutUS = () => {
           <Navigation />
         </Flex>
       </Container>
-      <div>
-        <h1>About us </h1>
-        <div>
-          <p>
-            Welcome to UrbanStyle, an avant-garde e-commerce destination where
+      <Container className="com:container com:mx-auto mb-32 sm:px-[10%]">
+        <h1 className="text-6xl font-semibold mb-5 ">About us </h1>
+        <div className="leading-loose text-xl sm:text-base sm:leading-loose">
+          <p className="mb-10">
+            <span className="capitalize text-3xl font-semibold ">W</span>
+            elcome to UrbanStyle, an avant-garde e-commerce destination where
             fashion converges with urban flair to redefine your style journey.
             At UrbanStyle, we pride ourselves on curating an eclectic collection
             that transcends conventional boundaries, offering a diverse range of
@@ -38,33 +47,54 @@ export const AboutUS = () => {
             item meticulously chosen to cater to a spectrum of tastes and
             preferences.
           </p>
-          <p>
-            What sets UrbanStyle apart is our commitment to quality. Every
-            garment undergoes rigorous scrutiny to ensure it meets the highest
-            standards of craftsmanship and durability. We collaborate with
-            cutting-edge designers and emerging talent to bring you exclusive
-            collections that resonate with the ever-evolving landscape of
-            fashion.
-          </p>{" "}
-          <p>
-            {" "}
-            UrbanStyle isn't just a shopping platform; it's a lifestyle. By
-            joining our community, you become part of a global network of
-            fashion enthusiasts who appreciate the fusion of style, comfort, and
-            urban sophistication. Stay connected with the latest fashion news,
-            style tips, and exclusive promotions through our newsletters and
-            social media channels, and let UrbanStyle be your go-to destination
-            for all things fashion.
-          </p>{" "}
-          <p>
-            {" "}
-            Embark on a style revolution with UrbanStyle – where the streets
-            meet the runway, and your fashion story begins. Explore, express,
-            and elevate your wardrobe with us. Welcome to a world where every
-            click brings you closer to the epitome of urban chic.
-          </p>
+
+          <div>
+            {showMore ? (
+              <>
+                <p className="mb-10">
+                  What sets UrbanStyle apart is our commitment to quality. Every
+                  garment undergoes rigorous scrutiny to ensure it meets the
+                  highest standards of craftsmanship and durability. We
+                  collaborate with cutting-edge designers and emerging talent to
+                  bring you exclusive collections that resonate with the
+                  ever-evolving landscape of fashion.
+                </p>{" "}
+                <p className="mb-10">
+                  {" "}
+                  UrbanStyle isn't just a shopping platform; it's a lifestyle.
+                  By joining our community, you become part of a global network
+                  of fashion enthusiasts who appreciate the fusion of style,
+                  comfort, and urban sophistication. Stay connected with the
+                  latest fashion news, style tips, and exclusive promotions
+                  through our newsletters and social media channels, and let
+                  UrbanStyle be your go-to destination for all things fashion.
+                </p>{" "}
+                <p className="mb-10">
+                  {" "}
+                  Embark on a style revolution with UrbanStyle – where the
+                  streets meet the runway, and your fashion story begins.
+                  Explore, express, and elevate your wardrobe with us. Welcome
+                  to a world where every click brings you closer to the epitome
+                  of urban chic.
+                </p>
+                <Button
+                  onClick={toggleShowMore}
+                  className="bg-Charcoal-Gray text-white px-5 py-2 rounded-md"
+                >
+                  Read Less
+                </Button>
+              </>
+            ) : (
+              <Button
+                onClick={toggleShowMore}
+                className="bg-Charcoal-Gray text-white px-5 py-2 rounded-md"
+              >
+                Read More
+              </Button>
+            )}
+          </div>
         </div>
-      </div>
+      </Container>
       <footer>
         <Footer />
       </footer>
